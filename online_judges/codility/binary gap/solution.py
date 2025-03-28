93c4f6b022fd6c6fb4408f34ef70710d6788d53f
Hong <hong@ubuntu-lab-01>
2025-03-28 18:49:10 +0800

    init

---
+++ online_judges/codility/binary gap/solution.py
@@ -0,0 +1,16 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N):    
+    # return len(max(format(N,'b').strip('0').split('1')))
+    return len(max(format(N,'b').strip('0').split('1')))
+
+def main():
+    print(solution(9))
+    print(solution(529))
+    print(solution(20))
+    print(solution(15))
+    print(solution(32))
+
+if __name__ == "__main__":
+    main()
